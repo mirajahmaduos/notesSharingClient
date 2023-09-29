dd212cfdf5a48c4dcb0246d89f14fe2c82911581
mirajahmaduos <mirajahmaduae@gmail.com>
2023-09-29 10:00:24 +0500

    mynotes updated

--- src/Components/MyNotes/mynotes.js
+++ src/Components/MyNotes/mynotes.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import UserNavbar from "../Navbar/usernavbar";
-import "../ViewAllnotes/css.css"
+import "../ViewAllnotes/css.css";
 
 export default function Mynotes() {
+ const [notes, setNotes] = useState([]);
+ useEffect(() => {
+    const userId = localStorage.getItem('user');
+    console.log('userId', userId);
+    const fetchData = () => {
+        fetch(`http://localhost:5000/mynotes/${userId}`)
+        .then((response) => {
+            if(!response.ok){
+                throw new Error('Notes data not gotted');
+            }
+            // console.log('response', response.json());
+            // setNotes(response.json());
+            return response.json();
+        })
+        .then(data => setNotes(data))
+        .catch((err) => {
+            console.log('error in getting mynotes', err);
+        })
+    }
+    fetchData();
+ }, []);
+ console.log('notes in state', notes);
+ const handleDownload = (downloadLink) => {
+    window.location.href = downloadLink;
+  };
     return (
         <>
             <UserNavbar />
@@ -27,7 +52,21 @@ export default function Mynotes() {
                         </thead>
 
                         <tbody>
-
+                            {notes.map((note, index) => (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{note.user.name}</td>
+                                    <td>{note.uploadingDate}</td>
+                                    <td>{note.branch}</td>
+                                    <td>{note.subject}</td>
+                                    <td> <button onClick={() => handleDownload(note.filePath)}>
+                                        Download <a href={note.filePath} download></a>
+                                        </button></td>
+                                    <td>{note.fileType}</td>
+                                    <td>{note.description}</td>
+                                    <td>{note.status}</td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
